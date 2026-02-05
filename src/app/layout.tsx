6a2f02d18ecc4e23cd41e8f4f7881f8fddbefe2c
Gustavo Fermino Uessler <gustavouessler@gmail.com>
2026-02-05 19:08:39 -0300

    chore: update CNAME and enhance metadata in root layout with Open Graph and Twitter card details

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,28 @@ const ibmPlexSans = IBM_Plex_Sans({
 
 export const metadata: Metadata = {
   title: "Uessler.com",
-  description: "Portfolio operating system",
+  description: "Uessler OS",
+  openGraph: {
+    title: "Uessler.com",
+    description: "Uessler OS",
+    url: "https://uessler.com",
+    siteName: "Uessler.com",
+    images: [
+      {
+        url: "/icon.png",
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Uessler.com",
+    description: "Uessler OS",
+    images: ["/icon.png"],
+  },
 };
 
 export default function RootLayout({
